45b13ac1e4bd2f5c62a21c7b3abe86da5af54e7c
Yuya-Ito325 <c1004072@planet.kanazawa-it.ac.jp>
2021-08-15 02:54:01 +0900

    基本機能完成

--- otona_no_shateki/src/conponent/tail.js
+++ otona_no_shateki/src/conponent/tail.js
@@ -1,21 +1,26 @@
-import React, {useState,} from 'react';
+import React, {useState, useEffect} from 'react';
 
 const Tail = () =>{
 
-  const [contentChild, setContentChild ]= useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]);
+  const child = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+  
+  const [array , setArray] = useState([]);
 
-
-
-  const doAction = (index) =>{
-    let copyContentChild = [...contentChild]
-    
+  const doChange = (i) => {
+      let copyArray = [...array]
+      copyArray.push(i)
+      setArray(copyArray)
   }
+
+
+  
+  
 
   return(
     <React.Fragment>
       <div className='content_palent' id='target'>
-        {contentChild.map((i) => (
-          <button key={i} className={contentChild.includes('i') ? 'on' : 'off'} onClick={doAction(i)}></button>
+        {child.map((i) => (
+          <button key={i}　  className={array.includes(i) ? 'off':'on'} onClick={() =>doChange(i)}></button>
         ))}
       </div>
     </React.Fragment>
